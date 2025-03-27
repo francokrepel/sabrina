417e34869f8c15e8d91f77a4a446c1f5ef52e8d6
francokrepel <73900300+francokrepel@users.noreply.github.com>
2025-03-26 21:13:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import functions
 from packaging import version
 
 # required_version = version.parse("1.1.1")
-current_version = version.parse(openai.__version__)
+# current_version = version.parse(openai.__version__)
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 # if current_version < required_version:
 #   raise ValueError(
